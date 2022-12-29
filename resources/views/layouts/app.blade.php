7265028b6aa013e99b770a0bc45982ea3b7e7e04
pb98ns <61550048+pb98ns@users.noreply.github.com>
2022-12-29 13:19:13 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,7 +12,7 @@
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
@@ -41,7 +41,7 @@
             
                 <a class="navbar-brand mx-auto" href="{{ url('/home') }}" >
               
-                    <img class="img-fluid mx-auto d-block" src="bbc.png" alt="" width="55" height="25"  >  
+                    <img class="img-fluid mx-auto d-block" src="{{URL::asset('bbc.png')}}" alt="" width="55" height="25"  >  
                 
                    
                 </a>
